6191a26ae71acda15dc56ee458130a285c4622e2
altwine <180307749+altwine@users.noreply.github.com>
2025-05-14 22:03:37 +0300

    Rename .md files to README's, lab2 matrices equality check

---
+++ lab2/README.md
@@ -0,0 +1,9 @@
+# Лабораторная работа 2
+
+Результаты запуска:
+```
+Matrix 1: 253.798 MFlops (67.691s)
+Matrix 2: 65075.3 MFlops (0.264s)
+Matrix 3: 1575.41 MFlops (10.905s)
+Matrices are equal
+```
--- lab2/lab2.cxx
+++ lab2/lab2.cxx
@@ -39,21 +39,27 @@ void multiplyMatrices3(int n, const complex<double>* A, const complex<double>* B
     for (int i = 0; i < n * n; i++)
         C[i] = 0;
     for (int i = 0; i < n; i += chunk)
-        for (int j = 0; j < n; j += chunk)
-            for (int k = 0; k < n; k += chunk) {
-                int i_end = min(i + chunk, n);
-                int j_end = min(j + chunk, n);
-                int k_end = min(k + chunk, n);
-                for (int ii = i; ii < i_end; ii++) {
-                    for (int jj = j; jj < j_end; jj++) {
-                        complex<double> sum = 0;
-                        for (int kk = k; kk < k_end; kk++) {
-                            sum += A[ii * n + kk] * B[kk * n + jj];
-                        }
-                        C[ii * n + jj] += sum;
-                    }
-                }
-            }
+    for (int j = 0; j < n; j += chunk)
+    for (int k = 0; k < n; k += chunk) {
+        int i_end = min(i + chunk, n);
+        int j_end = min(j + chunk, n);
+        int k_end = min(k + chunk, n);
+        for (int ii = i; ii < i_end; ii++) for (int jj = j; jj < j_end; jj++) {
+            complex<double> sum = 0;
+            for (int kk = k; kk < k_end; kk++)
+                sum += A[ii * n + kk] * B[kk * n + jj];
+            C[ii * n + jj] += sum;
+        }
+    }
+}
+
+bool validateMatrices (const complex<double>* C_1, const complex<double>* C_2, const complex<double>* C_3) {
+    for (int i = 0; i < n * n; i++){
+        bool cond1 = abs(C_1[i].real() - C_2[i].real()) > 1e-6 || abs(C_1[i].imag() - C_2[i].imag()) > 1e-6;
+        bool cond2 = abs(C_1[i].real() - C_3[i].real()) > 1e-6 || abs(C_1[i].imag() - C_3[i].imag()) > 1e-6;
+        if (cond1 || cond2) return false;
+    }
+    return true;
 }
 
 double toMFlops (int64_t duration) {
@@ -65,30 +71,41 @@ int main() {
     // Создаю три матрицы, первые два заполняю случ. числами
     complex<double> *A = new complex<double>[n * n];
     complex<double> *B = new complex<double>[n * n];
-    complex<double> *C = new complex<double>[n * n];
+    complex<double> *C_1 = new complex<double>[n * n];
+    complex<double> *C_2 = new complex<double>[n * n];
+    complex<double> *C_3 = new complex<double>[n * n];
     fillMatrix(n, A);
     fillMatrix(n, B);
 
     // Первый способ
     auto start = high_resolution_clock::now();
-    multiplyMatrices1(n, A, B, C);
+    multiplyMatrices1(n, A, B, C_1);
     auto duration = duration_cast<milliseconds>(high_resolution_clock::now() - start);
-    cout << "Matrix 1: " << toMFlops(duration.count()) << "MFlops (" << duration.count() / 1000.0 <<  "s)" << endl;
+    cout << "Matrix 1: " << toMFlops(duration.count()) << " MFlops (" << duration.count() / 1000.0 <<  "s)" << endl;
 
     // Второй
     start = high_resolution_clock::now();
-    multiplyMatrices2(n, A, B, C);
+    multiplyMatrices2(n, A, B, C_2);
     duration = duration_cast<milliseconds>(high_resolution_clock::now() - start);
-    cout << "Matrix 2: " << toMFlops(duration.count()) << "MFlops (" << duration.count() / 1000.0 <<  "s)" << endl;
+    cout << "Matrix 2: " << toMFlops(duration.count()) << " MFlops (" << duration.count() / 1000.0 <<  "s)" << endl;
 
     // Третий
     start = high_resolution_clock::now();
-    multiplyMatrices3(n, A, B, C);
+    multiplyMatrices3(n, A, B, C_3);
     duration = duration_cast<milliseconds>(high_resolution_clock::now() - start);
-    cout << "Matrix 3: " << toMFlops(duration.count()) << "MFlops (" << duration.count() / 1000.0 <<  "s)" << endl;
+    cout << "Matrix 3: " << toMFlops(duration.count()) << " MFlops (" << duration.count() / 1000.0 <<  "s)" << endl;
+
+    // Сравниваю три получившихся матрицы
+    if (validateMatrices(C_1, C_2, C_3)) {
+        cout << "Matrices are equal" << endl;
+    } else {
+        cout << "Matrices are not equal" << endl;
+    }
 
     delete[] A;
     delete[] B;
-    delete[] C;
+    delete[] C_1;
+    delete[] C_2;
+    delete[] C_3;
     return 0;
 }
